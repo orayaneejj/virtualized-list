028193a2686f5a9b071bd191893a839d2c2b5dea
Orayanee Janjaeng <orayaneejj@gmail.com>
2025-02-13 22:45:29 +0700

    feat: add navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
 import ProductList from "./components/product-list";
+import Navbar from "./components/navbar";
 function App() {
   return (
     <>
+      <Navbar />
       <ProductList />
     </>
   );
---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <nav className="bg-[#f6f8fa] p-6 mb-2 w-full shadow-lg flex items-center justify-center">
+      <div className="w-7xl">
+        <span className="text-2xl font-bold">
+          CR<span className="text-amber-400">.</span>
+        </span>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
